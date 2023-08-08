abd85a47d5e38814568c49f2dc0ea3c0d92a4601
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-08-08 15:43:43 -0500

    fixed the issue of module not being found by reverting back to older version and fixed sytax error and async await that was missing await

---
+++ routes/api/category-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+ 
+  // find all categories
+  // Find all users
+  try{
+    const categories = await Category.findAll({
+      include:[{model:Product}]
+    });
+    res.status(200).json(categories);
+  }catch (error){
+   res.status(500).json({error:"internal server error"});
+  }
+  
+  // be sure to include its associated Products
+});
+
+// '/api/categories/?
+router.get('/:id', async (req, res) => {
+  // we CAPTURE the dynamic part of the incoming REQUEST (req.params.id)
+  let categoryId = req.params.id;
+  // find one category by its `id` value
+  try{ 
+    let currentCategory = await Category.findOne({ 
+      where: { id: req.params.id },
+      // be sure to include its associated Products
+      include: [Product]
+    });
+    console.log("Data: ", currentCategory);
+    res.json(currentCategory);
+  } catch(error) {
+    res.status(500).json({error:"internal server error"});
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  let userInput = req.body;  // { category_name: "bingo" }
+
+  let newCategory = await Category.create(req.body);
+
+ //  res.json(newCategory);
+  res.json({ msg: "Category Created Successfully"});
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      
+    }
+  })
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try{
+    const catDelete = await Category.destroy({
+      where: {id: req.params.id}
+    });
+    if(!catDelete) {
+      res.status(404).json({message: 'No category with this id'});
+      return;
+    }
+    res.status(200).json(catDelete)
+  }catch(error) {
+    res.status(500).json({error:"internal server error"});
+  }
+});
+
+module.exports = router;
