e81707bfb67dbdd5416e4f4d9b31d775b372c523
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-08-09 21:30:10 -0500

    removed code that was not needed at all

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,12 +38,6 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const productData = await Product.create(req.body);
-    res.status(200).json(productData);
-  } catch(err) {
-    res.status(400).json(err);
-  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -115,6 +109,7 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
+
 
 router.delete('/:id', async (req, res) => {
   try {
